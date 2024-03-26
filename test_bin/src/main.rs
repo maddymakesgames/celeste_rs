a4218046fa643be98ccb68ecc3a592a52333e8b1
maddymakesgames <madelinemakesgames@gmail.com>
2024-03-26 12:55:48 -0400

    make test bin take a path to the file you want to test r/w

--- test_bin/src/main.rs
+++ test_bin/src/main.rs
@@ -2,11 +2,11 @@ use celeste_rs::saves::SaveData;
 use std::{fs::OpenOptions, io::BufReader};
 
 fn main() -> std::io::Result<()> {
-    let file = OpenOptions::new()
-        .read(true)
-        .write(false)
-        .open("../assets/0.celeste")?;
-
+    let mut args = std::env::args();
+    args.next().unwrap();
+    let path = args.next().unwrap();
+    println!("{path}");
+    let file = OpenOptions::new().read(true).write(false).open(path)?;
     let document = &mut quick_xml::de::Deserializer::from_reader(BufReader::new(file));
     let document: SaveData = match serde_path_to_error::deserialize(document) {
         Ok(a) => a,
